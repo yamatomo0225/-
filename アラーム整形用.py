415675fbcd204fd3d717a1db2caf5ea9ced18a78
Tomoya Yamazaki <yamatomo0225@gmail.com>
2023-09-03 09:41:47 +0900

    2023年9月3日

--- アラーム整形用.py
+++ アラーム整形用.py
@@ -2,63 +2,81 @@ import pandas as pd
 import tkinter as tk
 
 
-######Frotigate処理用######
-df = pd.read_csv("D:fortigate.csv",names=["rows"])
-
-#「"」で囲まれているデータは「＊＊＊="([^"]*)"\s+」で抽出
-#その他は、「time=(.*?)\s+'」で抽出
-
-df["date"]=df["rows"].str.extract(r'date=(.*?)\s+')
-df["time"]=df["rows"].str.extract(r'time=(.*?)\s+')
-
-df["type"]=df["rows"].str.extract(r'type="([^"]*)"\s+')
-df["level"]=df["rows"].str.extract(r'level="([^"]*)"\s+')
-df["attack"]=df["rows"].str.extract(r'attack="([^"]*)"\s+')
-df["msg"]=df["rows"].str.extract(r'msg="([^"]*)"\s+')
-df["action"]=df["rows"].str.extract(r'action="([^"]*)"\s+')
-df["srcip"]=df["rows"].str.extract(r'srcip=(.*?)\s+')
-df["srcport"]=df["rows"].str.extract(r'srcport=(.*?)\s+')
-df["dstip"]=df["rows"].str.extract(r'dstip=(.*?)\s+')
-df["dstport"]=df["rows"].str.extract(r'dstport=(.*?)\s+')
-df["ref"]=df["rows"].str.extract(r'ref="([^"]*)"\s+')
-
-df["datetime"] = pd.to_datetime(df["date"]+"T"+df["time"])
-df["datetime"] = df["datetime"].dt.strftime('%Y/%m/%d %H:%M:%S')
-df["route"] = df["srcip"]+"→"+df["dstip"]+"(port:"+df["dstport"]+")"
-df["srcip_unique"] = df["route"].str.extract(r'(.*?)→')
-df["dstip_unique"] = df["route"].str.extract(r'→(.*?\(port:.*?\))')
-
-print("■日時")
-min_date = str(df['datetime'].min())[:10]
-max_date = str(df['datetime'].max())[:10]
-
-if max_date == min_date:
-    M_datetime = str(df['datetime'].min())+"～"+str(df['datetime'].max()[12:])+"("+str(df['datetime'].count()-1)+"件)"
-    print(M_datetime)
-else:
-    M_datetime = str(df['datetime'].min())+"～"+str(df['datetime'].max())+"("+str(df['datetime'].count()-1)+"件)"
-    print(M_datetime)
-
-print("■イベント名")
-for unique_list in df['attack'].unique():
-    print(unique_list)
-
-print("■通信経路")
-for unique_list in df["route"].unique():
-    print(unique_list)
-
-print("■送信元")
-for unique_list in df['srcip_unique'].unique():
-    print(unique_list)
+def push_reset():
+    text_input.delete(0.0,"end")
+def push_start():
+    radio_value = radio_0.get()
+    text =text_input.get(0.0,"end")
     
-print("■送信先")
-for unique_list in df["dstip_unique"].unique():
-    print(unique_list)
+#FortiGate処理用
+    if radio_value == "FortiGate":
+        df = pd.read_csv("D:fortigate.csv",names=["rows"])
 
+        #「"」で囲まれているデータは「＊＊＊="([^"]*)"\s+」で抽出
+        #その他は、「time=(.*?)\s+'」で抽出
+
+        df["date"]=df["rows"].str.extract(r'date=(.*?)\s+')
+        df["time"]=df["rows"].str.extract(r'time=(.*?)\s+')
+        df["type"]=df["rows"].str.extract(r'type="([^"]*)"\s+')
+        df["level"]=df["rows"].str.extract(r'level="([^"]*)"\s+')
+        df["attack"]=df["rows"].str.extract(r'attack="([^"]*)"\s+')
+        df["msg"]=df["rows"].str.extract(r'msg="([^"]*)"\s+')
+        df["action"]=df["rows"].str.extract(r'action="([^"]*)"\s+')
+        df["srcip"]=df["rows"].str.extract(r'srcip=(.*?)\s+')
+        df["srcport"]=df["rows"].str.extract(r'srcport=(.*?)\s+')
+        df["dstip"]=df["rows"].str.extract(r'dstip=(.*?)\s+')
+        df["dstport"]=df["rows"].str.extract(r'dstport=(.*?)\s+')
+        df["ref"]=df["rows"].str.extract(r'ref="([^"]*)"\s+')
+
+        #統一規格
+
+        df["datetime"] = pd.to_datetime(df["date"]+"T"+df["time"])
+        df["datetime"] = df["datetime"].dt.strftime('%Y/%m/%d %H:%M:%S')
+        df["route"] = df["srcip"]+"→"+df["dstip"]+"(port:"+df["dstport"]+")"
+        df["route_unique_srcip"] = df["route"].str.extract(r'(.*?)→')
+        df["route_unique_dstip_port"] = df["route"].str.extract(r'→(.*?\(port:.*?\))')
+
+
+#メール出力用
+
+        min_date = str(df['datetime'].min())[:10]
+        max_date = str(df['datetime'].max())[:10]
+        if max_date == min_date:
+            M_datetime = str(df['datetime'].min())+"～"+str(df['datetime'].max()[12:])+"("+str(df['datetime'].count()-1)+"件)"
+        else:
+            M_datetime = str(df['datetime'].min())+"～"+str(df['datetime'].max())+"("+str(df['datetime'].count()-1)+"件)"
+        attack_list=""
+        for i in df['attack'].unique():
+            attack_list = str(attack_list) + str(i) + "\n"
+        route_list=""
+        for i in df["route"].unique():
+            route_list = str(route_list) + str(i) + "\n"
+        srcip_list=""
+        for i in df['route_unique_srcip'].unique():
+            srcip_list = str(srcip_list) + str(i) + "\n"
+        dstip_port_list=""
+        for i in df["route_unique_dstip_port"].unique():
+            dstip_port_list = str(dstip_port_list) + str(i) + "\n"
+
+        text_output.insert(
+            0.0,
+            '■検知時刻\n'
+            +M_datetime+"\n\n"
+            '■イベント名\n'
+            +attack_list+"\n"
+            '■通信経路\n'
+            +route_list+"\n"
+            '■送信元IP\n'
+            +srcip_list+"\n"
+            '■宛先IP(Port)\n'
+            +dstip_port_list+"\n"
+        )
+
+#GUI
 
 root = tk.Tk()
 root.title(u"Software Title")
-root.geometry("400x300")
+root.geometry("1000x600")
 
 radio_0 = tk.StringVar(value="FortiGate")
 
@@ -115,6 +133,7 @@ radio_iMark.grid(
 button_read = tk.Button(
     root,
     text="Start",
+    command=push_start
 )
 button_read.grid(
     row=2,
@@ -123,7 +142,7 @@ button_read.grid(
 button_reset = tk.Button(
     root,
     text="Reset",
-    #command=
+    command=push_reset
 )
 button_reset.grid(
     row=2,
@@ -140,8 +159,8 @@ text_input.grid(
 )
 text_output=tk.Text(
     root,
-    width=50,
-    height=5,
+    width=100,
+    height=100,
 )
 text_output.grid(
     row=4,
